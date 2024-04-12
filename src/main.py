868fda95e0fe5dffde0329de681463d2508720e8
Catalina <202104665@alu.comillas.edu>
2024-04-12 12:59:05 +0200

    main.py: flake8 + rename of the data folder + set_seed included

--- src/main.py
+++ src/main.py
@@ -4,18 +4,29 @@ from src.model import MyModel
 from src.train import train_model
 from src.utils import load_data
 
-# Libraries to show the image and add the caption
-from PIL import Image
-import matplotlib.pyplot as plt
+# # Libraries to show the image and add the caption
+# from PIL import Image
+# import matplotlib.pyplot as plt
 
+from src.utils import set_seed
 
+# set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# set all seeds
+set_seed(42)
+
+# static variables
+DATA_PATH: str = "data"
+
 need_to_train = True
 
-def main():
-    
-    path = 'data/Flicker8k_Dataset'
 
+def main():
+    """
+    This function is the main program. It downloads the data, preprocesses it,
+    trains the model, and evaluates it.
+    """
     # Define hyperparameters
     batch_size = 32
     num_epochs = 10
@@ -26,9 +37,8 @@ def main():
     drop_prob = 0.5
 
     # load data
-    # TODO: implementar función load_data que cree los datasets y los dataloaders
-    train_loader, val_loader, test_loader, word_to_index, index_to_word = load_data(path, batch_size)
-
+    (train_loader, val_loader, test_loader,
+     word_to_index, index_to_word) = load_data(DATA_PATH, batch_size)
 
     if need_to_train:
         encoder_params = {'embedding_dim': embedding_size}
@@ -45,23 +55,25 @@ def main():
         # train model
         train_model(device, num_epochs, learning_rate, model, train_loader, val_loader)
 
+        # TODO: Implement the evaluation of the model
+
     else:
         # We load the model from the models folder
         model = torch.load('models/model.pth')
 
-    # We can now use the model to generate captions for new images
-    # Since images from the folder have already been preprocessed, we can use the following code
-    # to generate captions for them
+    # XXX: See if this is necessary and if not, remove it
+    # # We can now use the model to generate captions for new images
+    # # Since images from the folder have already been preprocessed, we can use the
+    # # following code to generate captions for them:
     # image = Image.open('path/to/image')
     # caption = model.generate_caption(image)
     # print(caption)
 
-    # We add the caption to the image and show it
+    # # We add the caption to the image and show it
     # plt.imshow(image)
     # plt.title(caption)
     # plt.show()
 
 
-
 if __name__ == "__main__":
-    main() 
+    main()
