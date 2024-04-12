9df9badb33673fa4d965670f810dc40ccdb594ba
Catalina <202104665@alu.comillas.edu>
2024-04-12 12:48:04 +0200

    in data.py: delete commented classes unused and corrected docstring

--- src/data.py
+++ src/data.py
@@ -1,125 +1,11 @@
 # deep learning libraries
 import torch
-from torch.utils.data import Dataset, DataLoader, random_split
+from torch.utils.data import Dataset
 from torchvision import transforms
 
 # other libraries
 import os
-import requests
-import tarfile
-import shutil
-from requests.models import Response
 from PIL import Image
-
-# from our libraries
-# FIXME: See how this function is called and, if necessary, create it
-# from src.text_data_processing import load_captions
-
-
-# class MSCOCODataset(Dataset):
-#     """
-#     This class is the MSCOCO Dataset.
-#     """
-
-#     def __init__(self, path: str, captions_dict: dict) -> None:
-#         """
-#         Constructor of MSCOCODataset.
-
-#         Args:
-#             path: path of the dataset.
-#             captions_dict: dictionary with the captions.
-#         """
-
-#         # set attributes
-#         # TODO: Verify if this is correct or pertinent
-#         self.path = path
-#         self.names = os.listdir(path)
-#         self.captions = captions_dict
-
-#     def __len__(self) -> int:
-#         """
-#         This method returns the length of the dataset.
-
-#         Returns:
-#             length of dataset.
-#         """
-#         # TODO: Verify if this is correct or pertinent
-#         return len(self.names)
-
-#     def __getitem__(self, index: int) -> tuple[torch.Tensor, int]:
-#         """
-#         This method loads an item based on the index.
-
-#         Args:
-#             index: index of the element in the dataset.
-
-#         Returns:
-#             tuple with image and label. Image dimensions:
-#                 [channels, height, width].
-#         """
-#         # TODO: Verify if this is correct or pertinent
-#         # load image path and label
-#         image_path: str = f"{self.path}/{self.names[index]}"
-#         label: int = int(self.names[index].split("_")[0])
-
-#         # load image
-#         transformations = transforms.Compose([transforms.ToTensor()])
-#         image = Image.open(image_path)
-#         image = transformations(image)
-
-#         return image, label
-
-
-# class Flikr30kDataset(Dataset):
-#     """
-#     This class is the Flikr30k Dataset.
-#     """
-
-#     def __init__(self, path: str) -> None:
-#         """
-#         Constructor of Flikr30kDataset.
-
-#         Args:
-#             path: path of the dataset.
-#         """
-
-#         # set attributes
-#         # TODO: Flikr30kDataset Verify if this is correct or pertinent
-#         self.path = path
-#         self.names = os.listdir(path)
-
-#     def __len__(self) -> int:
-#         """
-#         This method returns the length of the dataset.
-
-#         Returns:
-#             length of dataset.
-#         """
-#         # TODO: Flikr30kDataset Verify if this is correct or pertinent
-#         return len(self.names)
-
-#     def __getitem__(self, index: int) -> tuple[torch.Tensor, int]:
-#         """
-#         This method loads an item based on the index.
-
-#         Args:
-#             index: index of the element in the dataset.
-
-#         Returns:
-#             tuple with image and label. Image dimensions:
-#                 [channels, height, width].
-#         """
-#         # TODO: Flikr30kDataset Verify if this is correct or pertinent
-#         # load image path and label
-#         image_path: str = f"{self.path}/{self.names[index]}"
-#         label: int = int(self.names[index].split("_")[0])
-
-#         # load image
-#         transformations = transforms.Compose([transforms.ToTensor()])
-#         image = Image.open(image_path)
-#         image = transformations(image)
-
-#         return image, label
 
 
 class ImageAndCaptionsDataset(Dataset):
@@ -127,9 +13,10 @@ class ImageAndCaptionsDataset(Dataset):
     This class is the Image and Captions Dataset.
 
     Attributes:
-        path: path of the dataset.
-        names: list with the names of the images.
-        captions: dictionary with the captions.
+        path (str): path of the dataset.
+        names (list): list with the names of the images.
+        captions (dict): dictionary with the captions, where the key is the
+        name of the image and the value is a list with the captions.
     """
 
     def __init__(self, path: str, captions_dict: dict) -> None:
@@ -137,10 +24,11 @@ class ImageAndCaptionsDataset(Dataset):
         Constructor of Flikr8kDataset.
 
         Args:
-            path: path of the dataset.
-            captions_dict: dictionary with the captions.
+            path (str): path of the dataset.
+            captions_dict (dict): dictionary with the captions, where the
+            key is the name of the image and the value is a list with the
+            captions.
         """
-        # TODO: Test this method
         self.path = path
         self.names = os.listdir(path)
         self.captions = captions_dict
@@ -150,23 +38,21 @@ class ImageAndCaptionsDataset(Dataset):
         This method returns the length of the dataset.
 
         Returns:
-            length of dataset.
+            int: length of the dataset.
         """
-        # TODO: Test this method
         return len(self.names)
 
-    def __getitem__(self, index: int) -> tuple[torch.Tensor, int]:
+    def __getitem__(self, index: int) -> tuple[torch.Tensor, torch.Tensor]:
         """
         This method loads an item based on the index.
 
         Args:
-            index: index of the element in the dataset.
+            index (int): index of the item to load.
 
         Returns:
-            tuple with image and label. Image dimensions:
-                [channels, height, width].
+            tuple[torch.Tensor, torch.Tensor]: tuple with the image and
+            the captions. Image dimensions: [channels, height, width].
         """
-        # TODO: Test this method
         # Load image path and captions
         image_path: str = f"{self.path}/{self.names[index]}"
         captions: list = self.captions[self.names[index]]
