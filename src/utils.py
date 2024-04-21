87915bedee2fa80290bd773fb12d6090bfd146a7
Maria <202110874@alu.comillas.edu>
2024-04-21 18:53:44 +0200

    images resize for the val and test dataset

--- src/utils.py
+++ src/utils.py
@@ -102,7 +102,7 @@ def load_data(
 
     transform_val_test = transforms.Compose(
         [
-            transforms.CenterCrop((299, 299)),
+            transforms.Resize((299, 299)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
